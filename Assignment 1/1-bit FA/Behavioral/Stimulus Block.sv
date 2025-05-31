d73f18b0ae2d962ce42092c9e16d267e70ffeb56
Divyansh Tiwari <u24ec063@eced.svnit.ac.in>
2025-05-31 23:45:02 +0530

    Update Stimulus Block.sv

--- Assignment 1/1-bit FA/Behavioral/Stimulus Block.sv
+++ Assignment 1/1-bit FA/Behavioral/Stimulus Block.sv
@@ -15,28 +15,28 @@ module FAtb;
     $display("A B Cin  S Cout");
     
     A=0; B=0; Cin=0; #10;
-    $display("%d %d %d    %d %d", A, B, Cin, S, Cout);
+    $display("%b %b %b    %b %b", A, B, Cin, S, Cout);
     
     A=0; B=0; Cin=1; #10;
-    $display("%d %d %d    %d %d", A, B, Cin, S, Cout); 
+     $display("%b %b %b    %b %b", A, B, Cin, S, Cout); 
     
     A=0; B=1; Cin=0; #10;
-    $display("%d %d %d    %d %d", A, B, Cin, S, Cout); 
+     $display("%b %b %b    %b %b", A, B, Cin, S, Cout);
     
     A=0; B=1; Cin=1; #10;
-    $display("%d %d %d    %d %d", A, B, Cin, S, Cout); 
+     $display("%b %b %b    %b %b", A, B, Cin, S, Cout);
     
     A=1; B=0; Cin=0; #10;
-    $display("%d %d %d    %d %d", A, B, Cin, S, Cout); 
+     $display("%b %b %b    %b %b", A, B, Cin, S, Cout); 
     
     A=1; B=0; Cin=1; #10;
-    $display("%d %d %d    %d %d", A, B, Cin, S, Cout); 
+     $display("%b %b %b    %b %b", A, B, Cin, S, Cout); 
     
     A=1; B=1; Cin=0; #10;
-    $display("%d %d %d    %d %d", A, B, Cin, S, Cout); 
+     $display("%b %b %b    %b %b", A, B, Cin, S, Cout);
     
     A=1; B=1; Cin=1; #10;
-    $display("%d %d %d    %d %d", A, B, Cin, S, Cout); 
+     $display("%b %b %b    %b %b", A, B, Cin, S, Cout); 
     
     $finish;
     
