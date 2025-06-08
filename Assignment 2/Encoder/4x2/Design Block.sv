f473c26190aa86353a9aaadae3489830ac6b801f
Divyansh Tiwari <u24ec063@eced.svnit.ac.in>
2025-06-08 22:50:03 +0530

    Create Design Block.sv

---
+++ Assignment 2/Encoder/4x2/Design Block.sv
@@ -0,0 +1,16 @@
+module Encoder (
+  input  [3:0] inpt,       
+  output reg [1:0] out   
+);
+
+always @(*) begin
+  case (1'b1)
+    inpt[0]: out = 2'b00;
+    inpt[1]: out = 2'b01;
+    inpt[2]: out = 2'b10;
+    inpt[3]: out = 2'b11;
+    default: out = 2'b00;  // Default value
+  endcase
+end
+
+endmodule
