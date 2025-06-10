7c20e9039953e508978a0818f0489adcdc917f03
Divyansh Tiwari <u24ec063@eced.svnit.ac.in>
2025-06-10 13:10:54 +0530

    Create Stimulus Block.sv

---
+++ Assignment 2/Logic Gates using 2x1 MUX/NAND Gate/Stimulus Block.sv
@@ -0,0 +1,23 @@
+module TestBench;
+  reg A, B;
+  wire Y;
+
+  NAND nandgate (.A(A),
+           .B(B),
+           .Y(Y));
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, TestBench);
+    
+    $display("A B  Y");
+    $monitor("%b %b  %b", A, B, Y);
+
+    A = 0; B = 0; #10;
+    A = 0; B = 1; #10;
+    A = 1; B = 0; #10;
+    A = 1; B = 1; #10;
+
+    $finish;
+  end
+endmodule
