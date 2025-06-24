37095daa24f19ae9b2b2087f5e425929344488e3
Divyansh Tiwari <u24ec063@eced.svnit.ac.in>
2025-06-24 12:07:37 +0530

    Create Design Block.sv

---
+++ Assignment 3/Array Multiplier/8-bit/Design Block.sv
@@ -0,0 +1,23 @@
+module ArrayMultiplier (
+    input  [7:0] A, B,
+    output [15:0] P
+);
+    wire [15:0] partialproducts [7:0]; 
+    
+    genvar i;
+    generate
+      for (i = 0; i < 8; i = i + 1) begin : products
+            assign partialproducts[i] = (A & {8{B[i]}}) << i;
+        end
+    endgenerate
+
+    assign P = partialproducts[0] +
+               partialproducts[1] +
+               partialproducts[2] +
+               partialproducts[3] +
+               partialproducts[4] +
+               partialproducts[5] +
+               partialproducts[6] +
+               partialproducts[7];
+
+endmodule
