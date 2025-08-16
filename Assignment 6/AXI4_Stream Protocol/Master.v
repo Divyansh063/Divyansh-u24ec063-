fa007fecc89d0ec7b69ca113f8b20bbf64570aa4
Divyansh Tiwari <u24ec063@eced.svnit.ac.in>
2025-08-16 20:40:23 +0530

    Create Master.v

---
+++ Assignment 6/AXI4_Stream Protocol/Master.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+
+module Master #
+(
+    parameter DATA_WIDTH = 8
+)
+(
+    input  wire                   clk,
+    input  wire                   resetn,   // active-low reset
+    output reg  [DATA_WIDTH-1:0]  tdata,
+    output reg                    tvalid,
+    input  wire                   tready
+);
+
+    reg [DATA_WIDTH-1:0] counter;
+
+    always @(posedge clk) begin
+        if (!resetn) begin
+            counter <= 0;
+            tdata   <= 0;
+            tvalid  <= 0;
+        end
+        else begin
+            if (!tvalid) begin
+                // Send first data
+                tdata  <= counter;
+                tvalid <= 1;
+            end
+            else if (tvalid && tready) begin
+                // Handshake done, send next data
+                counter <= counter + 1;
+                tdata   <= counter + 1;
+                tvalid  <= 1;
+            end
+        end
+    end
+
+endmodule
