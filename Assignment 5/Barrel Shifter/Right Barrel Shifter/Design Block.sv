07a73c91c26f432478383ae8d3a2ae285c912a36
Divyansh Tiwari <u24ec063@eced.svnit.ac.in>
2025-08-17 17:02:04 +0530

    Create Design Block.sv

---
+++ Assignment 5/Barrel Shifter/Right Barrel Shifter/Design Block.sv
@@ -0,0 +1,9 @@
+`timescale 1ns/1ps
+
+module barrel_shifter_right (
+    input  wire [31:0] data_in,    
+    input  wire [4:0]  shift_amt,      // Shift amount (0-31)
+    output wire [31:0] data_out    
+);
+    assign data_out = data_in >> shift_amt;  // Logical Right Shift
+endmodule
